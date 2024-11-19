ec583e06db5deffe0b70c90af887fab7a8f310f3
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-19 15:36:24 +0200

    New chnages

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('account.urls')),
-    path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
-    path('api/auth/social/', include('dj_rest_auth.registration.social_urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
