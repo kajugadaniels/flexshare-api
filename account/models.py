94adce2ea04cb9e761ec7b28fb926582e626712b
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-11-19 11:06:14 +0200

    Add user page

--- account/models.py
+++ account/models.py
@@ -1,3 +1,66 @@
+import os
 from django.db import models
+from account.managers import *
+from django.utils import timezone
+from django.utils.text import slugify
+from imagekit.processors import ResizeToFill
+from imagekit.models import ProcessedImageField
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+def user_image_path(instance, filename):
+    base_filename, file_extension = os.path.splitext(filename)
+    return f'users/user_{slugify(instance.name)}_{instance.phone_number}_{instance.email}{file_extension}'
+
+class User(AbstractBaseUser, PermissionsMixin):
+    ROLE_CHOICES = (
+        ('Client', 'Client'),
+        ('Vendor', 'Vendor'),
+    )
+
+    GENDER_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+
+    STATUS_CHOICES = (
+        ('Active', 'Active'),
+        ('Inactive', 'Inactive'),
+        ('Pending', 'Pending'),
+        ('Banned', 'Banned'),
+    )
+
+    name = models.CharField(max_length=255, null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(unique=True, max_length=20, null=True, blank=True)
+    national_id = models.CharField(max_length=20, unique=True, null=True, blank=True)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    country = models.CharField(max_length=255, default='Rwanda')
+    district = models.CharField(max_length=255, null=True, blank=True)
+    sector = models.CharField(max_length=255, null=True, blank=True)
+    cell = models.CharField(max_length=255, null=True, blank=True)
+    village = models.CharField(max_length=255, null=True, blank=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Active')
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Client')
+    image = ProcessedImageField(
+        upload_to=user_image_path,
+        processors=[ResizeToFill(1270, 1270)],
+        format='JPEG',
+        options={'quality': 90},
+        null=True,
+        blank=True
+    )
+    bio = models.TextField(blank=True, null=True)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'phone_number']
+
+    def __str__(self):
+        return f"{self.name}"
